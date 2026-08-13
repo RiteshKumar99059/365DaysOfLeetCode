af594ccea3a174481fbe6e9e455aba1c2d37b50b
Ritesh Kumar <riteshchaurasiya99059@gmail.com>
2026-08-13 15:06:38 +0530

    Time: 13 ms (52.08%), Space: 19.3 MB (94.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -12,7 +12,25 @@ class Solution:
 
         # return a == b
 
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        
+        count = {}
+
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in t:
+            if ch not in count:
+                return False
+            
+            count[ch] -= 1
+            
+            if count[ch] < 0:
+                return False
+
+        return True
 
 
 
